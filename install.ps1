41240456ef3a18e549dd6c5b8dc72a664a9e8272
Justin Caselman <justin.caselman@keysight.com>
2024-02-08 09:14:13 -0700

    add staging msys2 repo

--- install.ps1
+++ install.ps1
@@ -5,6 +5,13 @@ iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocola
 # MSYS2, the work environment
 choco install -y msys2 git
 
+$pacman_conf_path = "C:\tools\msys64\etc\pacman.conf"
+$staging_directive = "[staging]`nServer = https://repo.msys2.org/staging/`nSigLevel = Never`n`n"
+$(
+    $staging_directive
+    Get-Content -Raw $pacman_conf_path
+) | Set-Content $pacman_conf_path
+
 # Install Ansible
 C:\tools\msys64\usr\bin\bash --login -c 'pacman -S --needed --noconfirm ansible'
 
