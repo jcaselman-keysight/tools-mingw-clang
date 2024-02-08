575593596d4c9e40f81eb8e1f165b8fd8101d58a
Justin Caselman <justin.caselman@keysight.com>
2024-02-08 13:09:09 -0700

    tweaks for turnkey sensetalk building

--- install.ps1
+++ install.ps1
@@ -5,12 +5,16 @@ iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocola
 # MSYS2, the work environment
 choco install -y msys2 git
 
+# Add msys2 staging database
 $pacman_conf_path = "C:\tools\msys64\etc\pacman.conf"
 $staging_directive = "[staging]`nServer = https://repo.msys2.org/staging/`nSigLevel = Never`n`n"
 $(
     $staging_directive
     Get-Content -Raw $pacman_conf_path
 ) | Set-Content $pacman_conf_path
+
+# Download staging database
+C:\tools\msys64\usr\bin\bash --login -c 'pacman -Sy'
 
 # Install Ansible
 C:\tools\msys64\usr\bin\bash --login -c 'pacman -S --needed --noconfirm ansible'
